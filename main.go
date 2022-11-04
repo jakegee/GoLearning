512fd23cb5163624b48ac2f4dc02cd56f077186a
jacosmith <jacosmith@expediagroup.com>
2022-11-04 17:29:54 +0000

    practice update

--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	var congrats string
-	congrats = "Congratulations"
-	congrats += "!!!"
-	fmt.Println(congrats)
 
-	var challenge string = "What else can you do?"
-	fmt.Println(challenge)
+	var publisher, writer, artist, title string
+	var year, pageNumber uint
+	var grade float32
 
-	reminder := "Pratice is important!"
-	fmt.Println(reminder)
+	title = "Mr. GoToSleep"
+	writer = "Tracey Hatchet"
+	artist = "Jewel Tampson"
+	publisher = "DizzyBooks Publishing Inc."
+	year = 1997
+	pageNumber = 14
+	grade = 6.5
+
+	fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, "year written", year, "page number", pageNumber, "graded", grade)
+
+	title = "Epic Vol. 1"
+	writer = "Ryan N. Shawn"
+	artist = "Phoebe Paperclips"
+	publisher = "DizzyBooks Publishing Inc."
+	year = 2013
+	pageNumber = 160
+	grade = 9.0
+
+	fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, "year written", year, "page number", pageNumber, "graded", grade)
+
 }
